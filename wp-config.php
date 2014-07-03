a4aace076bcb9fdc6e960178e916c9d2b86758d7
Nuno Morgadinho <nuno@widgilabs.com>
2014-07-03 20:14:27 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'tiratema_wrd10');
+define('DB_NAME', '-');
 
 /** MySQL database username */
-define('DB_USER', 'tiratema_wrd10');
+define('DB_USER', '-');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'xPKriwjMMi');
+define('DB_PASSWORD', '-');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
